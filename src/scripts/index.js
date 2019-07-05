e51004f0e8682233a6cec772af8984954fd65fd4
Jasper Stam <jasper.stam@philips.com>
2019-07-05 19:50:05 +0200

    Add orbit controls for debugging

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -140,17 +140,39 @@ class WorldRenderer3d {
   constructor(container) {
     this.container = container;
   }
+
   createScene() {
     const { width, height } = this.container.getBoundingClientRect();
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    this.container.appendChild( renderer.domElement );
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( width, height );
+    this.container.appendChild( this.renderer.domElement );
   }
   
+  createMap() {
+    console.log('this.scene'), this.scene;
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    this.cube = new THREE.Mesh( geometry, material );
+    this.scene.add( this.cube );
+    this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+    this.camera.position.z = 5;
+  }
+
   render() {
     this.createScene();
+    this.createMap();
+
+    this.animate();
+  }
+  
+  animate() {
+    this.controls.update();
+    // this.cube.rotation.x += 0.01;
+    // this.cube.rotation.y += 0.01;
+    window.requestAnimationFrame(() => this.animate());
+    this.renderer.render(this.scene, this.camera );
   }
 }
 
