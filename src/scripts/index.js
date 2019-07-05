87b40b1401c234a24e6416ee8dd7dc646572512e
Jasper Stam <jasper.stam@philips.com>
2019-07-05 19:55:17 +0200

    Add hemisphere & directional lighting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -149,11 +149,38 @@ class WorldRenderer3d {
     this.renderer.setSize( width, height );
     this.container.appendChild( this.renderer.domElement );
   }
+
+  addLighting() {
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+    hemiLight.color.setHSL( 0.6, 1, 0.6 );
+    hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+    hemiLight.position.set( 0, 50, 0 );
+    this.scene.add( hemiLight );
+    const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+    this.scene.add( hemiLightHelper );
+
+    const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    dirLight.color.setHSL( 0.1, 1, 0.95 );
+    dirLight.position.set( - 1, 1.75, 1 );
+    dirLight.position.multiplyScalar( 30 );
+    this.scene.add( dirLight );
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+    var d = 50;
+    dirLight.shadow.camera.left = - d;
+    dirLight.shadow.camera.right = d;
+    dirLight.shadow.camera.top = d;
+    dirLight.shadow.camera.bottom = - d;
+    dirLight.shadow.camera.far = 3500;
+    dirLight.shadow.bias = - 0.0001;
+    const dirLightHeper = new THREE.DirectionalLightHelper( dirLight, 10 );
+    this.scene.add( dirLightHeper );
+  }
   
   createMap() {
-    console.log('this.scene'), this.scene;
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
     this.cube = new THREE.Mesh( geometry, material );
     this.scene.add( this.cube );
     this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
@@ -162,6 +189,7 @@ class WorldRenderer3d {
 
   render() {
     this.createScene();
+    this.addLighting();
     this.createMap();
 
     this.animate();
